e345fa4c4c08286b70d9e735d14d671bdb735fd1
Parasa <S561500@nwmissouri.edu>
2024-04-14 04:51:24 -0500

    usercontroller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -26,6 +26,8 @@ export const login =  catchAsyncError(async(req,res,next)=>{
         return next(new ErrorHandler("Provide email,password and role",400));
     }
 
+    console.log(req.body)
+
     const user = await User.findOne({email}).select("+password");
     if(!user){
         return next(new ErrorHandler("Invalid email address",400));
@@ -56,7 +58,7 @@ export const getUser = catchAsyncError((req,res,next)=>{
 
     const user = req.user;
     
-
+    console.log("user",user)
     res.status(200).json({
         success: true,
         user
