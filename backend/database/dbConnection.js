d160de264f54a6258c61ee46fe3b3a55bbab76c8
Parasa <S561500@nwmissouri.edu>
2024-04-15 03:18:56 -0500

    added

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const dbConnection = ()=>{
-    mongoose.connect(process.env.MONGO_URL,{dbName : "job_seeking"})
+    mongoose.connect(process.env.MONGO_URL)
     .then(()=>{
         console.log("Database Connected");
         
